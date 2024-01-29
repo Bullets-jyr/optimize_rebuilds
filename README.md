81e7f81046e9007fad3678d6333c1706d2e408d2
Bullets-jyr <jyr@bullets.co.kr>
2024-01-29 14:59:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Provider Scope
+
+- Optimized rebuilds using Provider Scope
---
+++ android/app/src/main/kotlin/com/example/optimize_rebuilds/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.optimize_rebuilds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
