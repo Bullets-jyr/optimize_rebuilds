cecb71f042c04405a8815e1d39a4aa3dc8a2c059
Bullets-jyr <jyr@bullets.co.kr>
2024-01-29 15:14:04 +0900

    70. Optimize Rebuilds - 2

---
+++ lib/items/item_list_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'item_list_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$itemListHash() => r'82134f8083c47f38d48ff639ef34a2a83e638cb0';
+
+/// See also [ItemList].
+@ProviderFor(ItemList)
+final itemListProvider =
+    AutoDisposeNotifierProvider<ItemList, List<String>>.internal(
+  ItemList.new,
+  name: r'itemListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$itemListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ItemList = AutoDisposeNotifier<List<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
